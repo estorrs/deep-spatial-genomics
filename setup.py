a963cad9417fe085376ff9fec797fb20aadd06a5
Erik <epstorrs@gmail.com>
2022-08-24 13:31:02 -0500

    new docker build

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'deep_spatial_genomics',
+    name = 'deep-spatial-genomics',
     packages = find_packages(exclude=['examples']),
-    version = '0.0.1',
+    version = '0.0.2',
     license='MIT',
     description = 'Deep Spatial Genomics',
     author = 'Erik Storrs',
