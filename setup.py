f821819622fb7c895d111d81433a5767504ce9b3
Erik <epstorrs@gmail.com>
2022-08-24 11:59:23 -0500

    different name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'deep-spatial-genomics',
+    name = 'deep_spatial_genomics',
     packages = find_packages(exclude=['examples']),
     version = '0.0.1',
     license='MIT',
